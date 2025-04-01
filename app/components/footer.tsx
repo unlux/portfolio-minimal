25c50971573b6ca830e559f7f7df883b5d548b8e
lakshay choudhary <lakshaychoudhary77712@gmail.com>
2025-04-01 11:38:32 +0530

    feat: add CV link to footer component

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
-        <li>
+        {/* <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -29,7 +29,7 @@ export default function Footer() {
             <ArrowIcon />
             <p className="ml-2 h-7">rss</p>
           </a>
-        </li>
+        </li> */}
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
@@ -62,6 +62,17 @@ export default function Footer() {
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedIn</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="/Lux-Resume.pdf"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">cv</p>
           </a>
         </li>
         <li>
